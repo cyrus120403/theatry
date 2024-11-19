b0af46642d95e555c158778e37e9b8de204d8694
athalialimbo <limboalthea8@gmail.com>
2024-11-19 10:31:09 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+//console.log(fetch('https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/51fe71a3-cb12-4ac2-882f-45955401dd53/Golden+Retrievers+dans+pet+care.jpeg?format=2500w')
+
+//.then(response => {
+//    console.log(response)
+//    return response.blob();
+//})
+
+//.then(blob =>{
+ //   console.log(blob)
+ //   document.querySelector('#myImage').src=URL.createObjectURL(blob)
+//})
+
+//.catch(error =>{
+ //   console.log(error)
+//})
+
+//)
+
+// id selector
+
+//id selector
+const content=document.querySelector("#content")
+
+//loading page
+window.addEventListener("load", () =>{
+    getUsers()
+})
+
+function getUsers(){
+    let html=""
+
+    //fetch("http://localhost:5000/api/members" , {mode: "cors"})
+    fetch("https://bscs3a-crud-api.onrender.com/api/members/api/members" , {mode: "cors"})
+    .then((response) =>{
+        console.log(response)
+        return response.json()
+    })
+    .then((data)=>{
+        console.log(data)
+        data.forEach((element) =>{
+            html += `<li>${element.first_name} ${element.last_name}</li>`
+        })
+
+        content.innerHTML = html
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
+
+}
